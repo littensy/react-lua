aa10f1f1ba51192777f5cb5cca58b1d17a209cfe
Richard <richerd.rdc@gmail.com>
2023-07-07 13:00:23 -0700

    feat: add react-roblox types

--- packages/react-roblox/src/index.d.ts
+++ packages/react-roblox/src/index.d.ts
@@ -1,1 +1,21 @@
-export {};
+import Roact from "@rbxts/react-ts";
+
+export interface RootOptions {
+	hydrate?: boolean;
+	hydrationOptions?: {
+		onHydrated?: (suspenseNode: any) => void;
+		onDeleted?: (suspenseNode: any) => void;
+		mutableSources?: any[];
+	};
+}
+
+export interface Root {
+	render(children: Roact.Element): void;
+	unmount(): void;
+}
+
+export function createRoot(container: Instance, options?: RootOptions): Root;
+
+export function createBlockingRoot(container: Instance, options?: RootOptions): Root;
+
+export function createLegacyRoot(container: Instance, options?: RootOptions): Root;
